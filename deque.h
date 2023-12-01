06aed80d56083fa98601ffb4ebd46f0f18bcb877
KevinBAustin <kevbaust@ut.utm.edu>
2023-12-01 15:55:44 -0600

    Declared most objects in header

--- deque.h
+++ deque.h
@@ -7,3 +7,24 @@
  * Declares all objects for the custom deque class
  */
 
+#ifndef DEQUE_H
+#define DEQUE_H
+
+class deque{
+ private:
+  int** blockmap;
+  
+ public:
+  deque();
+  ~deque();
+  void push_front{};
+  void push_back{};
+  void pop_front{};
+  void pop_back{};
+  int front();
+  int back();
+  bool empty();
+  int size();
+  
+};
+#endif
