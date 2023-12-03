1e29ddd8a4e14ab390f484dccb2a537e3cf18622
Matthew Lannom <matdlann@ut.utm.edu>
2023-12-02 19:58:52 -0600

    Commit so I can merge

--- deque.cpp
+++ deque.cpp
@@ -6,4 +6,135 @@
  * 
  * Defines all objects in the custom deque class
  */
+#include "deque.h"
+#include <iostream>
 
+using namespace std;
+
+void deque::resize(){
+  cout << "Resize called" << endl;
+  int ** temp;
+  mapsize+=2;
+  temp = new int* [mapsize];
+
+  temp[0] = new int[blocksize];
+
+  for (int i = 0; i < mapsize; i++){
+    temp[i+1] = blockmap[i];
+  }
+  temp[mapsize - 1] = new int[blocksize];
+  temp[mapsize - 2] = new int[blocksize];
+
+  delete[] blockmap;
+  blockmap = temp;
+}
+
+deque::deque(){
+  firstblock = 2;
+  firstelement = 2;
+  blockmap = new int* [mapsize];
+  for (int i = 0; i < mapsize; i++){
+    blockmap[i] = new int[blocksize];
+  }
+}
+
+
+deque::~deque(){
+  for (int i = 0; i < mapsize; i++){
+    delete[] blockmap[i];
+  }
+
+  delete[] blockmap;
+}
+
+void deque::push_front(int num){
+  if (firstelement == 0 && firstblock == 0){
+    resize();
+    firstblock = 1;
+    firstelement=blocksize-1;
+    blockmap[firstblock][firstelement] = num;
+    amount++;
+  }
+  else if (firstelement == 0){
+    blockmap[firstblock][firstelement] = num;
+    amount++;
+    firstblock = firstblock-1;
+    firstelement = blocksize;
+  }
+  else {
+    firstelement = firstelement-1;
+    blockmap[firstblock][firstelement] = num;
+    amount++;
+  }
+}
+
+void deque::push_back(int num){
+  int row;
+  int col;
+  row = firstblock + ((firstelement + amount)/blocksize);
+  col = (firstelement + amount) % blocksize;
+  if (amount == 0) {
+  blockmap[firstblock][firstelement] = num;
+  }
+  else if (row == mapsize-1){
+    blockmap[mapsize-1][blocksize-1] = num;
+    resize();
+  }
+  else if (col == blocksize-1){
+    blockmap[row][blocksize-1] = num;
+  }
+  else {
+    blockmap[row][col] = num;
+  }
+    amount++;
+}
+
+void deque::pop_front(){
+  if (firstblock == mapsize-1 && firstelement == blocksize - 1){
+    blockmap[firstblock][firstelement] = 0;
+    amount--;
+  }
+  else {
+    if (firstelement != blocksize-1){
+      blockmap[firstblock][firstelement] = 0;
+      amount--;
+      firstelement+=1;
+    }
+    else {
+      blockmap[firstblock][firstelement] = 0;
+      amount--;
+      firstblock+=1;
+    }
+  }
+}
+
+void deque::pop_back(){
+  
+}
+
+int deque::front(){
+  return blockmap[firstblock][firstelement];
+}
+
+int deque::back(){
+  int row;
+  int col;
+  row = firstblock + ((firstelement + amount)/blocksize);
+  col = (firstelement + amount) % blocksize;
+  return blockmap[row][col-1];
+}
+
+bool deque::empty(){
+}
+
+int deque::size(){
+  return amount;
+}
+
+int& deque::operator[](unsigned int index){
+  int row;
+  int col;
+  row = firstblock + ((firstelement + index)/blocksize);
+  col = (firstelement + index) % blocksize;
+  return blockmap[row][col];
+}
--- main.cpp
+++ main.cpp
@@ -7,3 +7,60 @@
  * 
  */
 
+
+#include <iostream>
+#include <fstream>
+#include "deque.h"
+
+using namespace std;
+
+int main() {
+  int num;
+  ifstream inFile;
+  string filename;
+  deque d;
+
+  cout << "Enter file name: " << endl;
+  cin >> filename;
+
+  inFile.open(filename);
+  inFile >> num;
+  int i = 0;
+  while(inFile){
+    d.push_front(num);
+    cout << d[0] << endl;
+    inFile >> num;
+  }
+  cout << endl << d[0] << endl;
+  inFile.close();
+  
+  //d[5] = 7;
+  //cout << d[5] << endl;
+  /*
+  d.push_front(6);
+  cout << d[0] << endl;
+  d.push_back(7);
+  cout << d[0] << endl;
+  d.push_front(34);
+  cout << d[0] << endl;
+  d.push_back(234);
+  d.push_front(42);
+  d.pop_front();
+  d.push_back(9000);
+  d.push_back(9001);
+  //d.pop_front();
+  //d.pop_front();
+  cout << d[0] << " " << d[1] << " " << d[2] << " " << d[3] << " " << d[4] << " " << d[5] << " " << d[6] << " " <<  d[7] << endl << endl;
+
+  cout << d.front() << endl;
+  cout << d.back() << endl;
+  cout << d.size() << endl;
+
+  
+  d.push_front(6);
+  cout << d[0] << endl;
+  d.pop_front();
+  cout << d[0] << endl;
+  */
+  return 0;
+}
